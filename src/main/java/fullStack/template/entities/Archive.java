5119259cefb7b3b224818ca45befa9605106c2bb
hamza <hamzaaitmansouri46@gmail.com>
2025-02-16 22:49:59 +0100

    "first commit to hamza"

---
+++ src/main/java/fullStack/template/entities/Archive.java
@@ -0,0 +1,25 @@
+package fullStack.template.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Archive {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int week;
+    private  int annee;
+
+    @OneToMany(mappedBy = "archive")
+    private  List<Presence> presences;
+
+}
--- src/main/java/fullStack/template/entities/Presence.java
+++ src/main/java/fullStack/template/entities/Presence.java
@@ -3,23 +3,20 @@ package fullStack.template.entities;
 import fullStack.template.models.UserApp;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 public class Presence {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Enumerated(EnumType.STRING)
-    private StatutPresence statut;
+    private String statut;
     private String remarque;
+    private LocalDate date;
 
     @ManyToOne
     private Seance seance;
     @ManyToOne
     private UserApp userApp;
 
-}
-
-enum StatutPresence {
-    PRESENT, ABSENT
 }
